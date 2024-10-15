a60db2e8d8e5539648e93741719e0f5f16705926
xTheFreeZe <marwin.eder@gmx.de>
2024-10-15 18:39:49 +0200

    KREUZ FERTIGGG

--- Bilder/Kreuzprodukt/kreuz.py
+++ Bilder/Kreuzprodukt/kreuz.py
@@ -16,28 +16,14 @@ class KreuzBild(ThreeDScene):
             z_axis_config={"color": BLUE},
         )
 
-        # y = [1]
-        # z 
+        # Die Koordinaten sind aus irgendeinem Grund gefickt
+        # z = [-, x, -]
+        # x = [-, -, x]
+        # y = [x, -, -]
 
-        vektor_eins = Arrow3D([0, 0, 0], [1, 0, 0], color=BLACK)
-        vektor_zwei = Arrow3D([0, 0, 0], [0, 1, 0], color=BLACK)
-        kreuzprodukt = Arrow3D([0, 0, 0], [0, 0, 1], color=BLACK)
-
-        tip_text = (
-            Text("(1, 0, 0)", color=BLACK)
-            .next_to(vektor_eins.get_end(), RIGHT)
-            .scale(0.5)
-        )
-        tip_text_zwei = (
-            Text("(0, 1, 0)", color=BLACK)
-            .next_to(vektor_zwei.get_end(), RIGHT)
-            .scale(0.5)
-        )
-        tip_text_kreuz = (
-            Text("(0, 0, 1)", color=BLACK)
-            .next_to(kreuzprodukt.get_end(), RIGHT)
-            .scale(0.5)
-        )
+        vektor_eins = Arrow3D([0, 0, 0], [0, 0, 1], color=BLACK)
+        vektor_zwei = Arrow3D([0, 0, 0], [1, 1, 0], color=BLACK)
+        kreuzprodukt = Arrow3D([0, 0, 0], [-1, 1, 0], color=BLUE)
 
         x_label = MathTex("y", color=BLACK).next_to(
             axes.x_axis.get_end(), RIGHT, buff=0.2
@@ -51,18 +37,14 @@ class KreuzBild(ThreeDScene):
             axes, vektor_eins, vektor_zwei, x_label, y_label, z_label
         )
 
-        # self.play(Create(vektor_eins), Create(vektor_zwei), Create(dot), run_time=1)
-        # play everything
-        self.play(
-            Create(axes),
-            Create(vektor_eins),
-            Create(vektor_zwei),
-            Create(kreuzprodukt),
-            Create(tip_text),
-            Create(tip_text_zwei),
-            Create(tip_text_kreuz),
-            run_time=2,
-        )
+        self.play(Create(axes))
+
+        self.wait(3)
+
+        self.play(Create(vektor_eins), Create(vektor_zwei), Create(kreuzprodukt))
+
+        self.wait(3)
+
         self.play(axis_and_lines.animate.rotate(angle=PI / 6, axis=UP))
         self.play(axis_and_lines.animate.rotate(angle=-PI / 12, axis=LEFT))
         self.wait(6)
